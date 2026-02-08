a6efb84846dfb2487ace318b55395efd1fd21110
ramirezleo_ <lgrg0497@gmail.com>
2026-02-08 20:12:51 -0300

    Actualizacion de logica

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print("Hola mundo desde mi primer repo")
-## Bienvenido
-Esta es la descripcion de mi primer repo.
+
+nombre = "Leonardo Gabriel"
+apellido = "Ramirez Gimenez"
+edad = 28
+
+print(nombre, apellido, edad);
